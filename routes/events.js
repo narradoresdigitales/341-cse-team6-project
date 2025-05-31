7f4f73c6857fcaa31954932d0f365a61fc901aad
sil <narradoresdigitales@gmail.com>
2025-05-31 13:07:06 -0500

    added express-validation to event PUT operation

--- routes/events.js
+++ routes/events.js
@@ -1,13 +1,18 @@
+const { body, validationResult } = require('express-validator');
 const express = require('express');
 const router = express.Router();
-
 const eventsController = require('../controllers/events');
+const {validateEvent} = require('../middleware/eventValidator');
+const handleValidationErrors = require('../middleware/validateResult');
+
+
+
 
 router.get('/', eventsController.getAll);
 
 router.get('/:id', eventsController.getSingle);
 
-router.post('/', eventsController.createEvent);
+router.post('/', validateEvent, handleValidationErrors, eventsController.createEvent);
 
 //router.put('/:id', eventsController.updateEvent);
 
