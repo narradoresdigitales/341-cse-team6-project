9cb8716e562bb5c6bb2f5f136945609682230842
jcampos-94 <jcampos.94@hotmail.com>
2025-06-04 16:55:44 -0500

    OAuth complete

--- routes/events.js
+++ routes/events.js
@@ -2,20 +2,46 @@ const { body, validationResult } = require('express-validator');
 const express = require('express');
 const router = express.Router();
 const eventsController = require('../controllers/events');
-const { validateEvent, validateEventId, validateUpdateEvent } = require('../middleware/eventValidator');
+const {
+  validateEvent,
+  validateEventId,
+  validateUpdateEvent,
+} = require('../middleware/eventValidator');
 const handleValidationErrors = require('../middleware/validateResult');
-
-
-
+const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/', eventsController.getAll);
 
-router.get('/:id', validateEventId, handleValidationErrors, eventsController.getSingle);
+router.get(
+  '/:id',
+  validateEventId,
+  handleValidationErrors,
+  eventsController.getSingle
+);
 
-router.post('/', validateEvent, handleValidationErrors, eventsController.createEvent);
+router.post(
+  '/',
+  isAuthenticated,
+  validateEvent,
+  handleValidationErrors,
+  eventsController.createEvent
+);
 
-router.put('/:id', validateEventId, validateUpdateEvent, handleValidationErrors, eventsController.updateEvent);
+router.put(
+  '/:id',
+  isAuthenticated,
+  validateEventId,
+  validateUpdateEvent,
+  handleValidationErrors,
+  eventsController.updateEvent
+);
 
-router.delete('/:id', validateEventId, handleValidationErrors, eventsController.deleteEvent);
+router.delete(
+  '/:id',
+  isAuthenticated,
+  validateEventId,
+  handleValidationErrors,
+  eventsController.deleteEvent
+);
 
-module.exports = router; 
+module.exports = router;
--- server.js
+++ server.js
@@ -1,35 +1,106 @@
+require('dotenv').config();
 const mongodb = require('./data/database');
 const express = require('express');
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
+app
+  .use(bodyParser.json())
+  .use(
+    session({
+      secret: 'secret',
+      resave: 'false',
+      saveUninitialized: true,
+    })
+  )
+  // init basic express session
+  .use(passport.initialize())
+  // init passport on every route call
+  .use(passport.session())
+  //allow passport to use 'express-session'
+  .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
     );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, post, PUT, DELETE, OPTIONS');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS'
+    );
     next();
-});
+  })
+  .use(cors({ methods: ['GET, POST, PUT, DELETE, OPTIONS'] }))
+  .use(cors({ origin: '*' }))
+  .use('/', require('./routes'));
 
-app.use('/', require('./routes'));
-
-process.on('uncaughtException', (err, origin) => {
-    console.log(process.stderr.id, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
-});
-
-mongodb.initDb((err) => {
-    if(err) {
-        console.log(err);
-    } else {
-        app.listen(port, () => { console.log(`Database is listening and node is running on port ${port}`)});
+// Passport
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      // accessToken and refreshToken are used by GitHub, even if the current function does not
+      return done(null, profile);
     }
-})
+  )
+);
 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
 
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
+// Endpoints
+app.get('/', (req, res) => {
+  // #swagger.tags=['341CSE Team 6 Project']
+  const loginStatus =
+    req.session.user !== undefined
+      ? `Logged in as ${req.session.user.username}`
+      : `Logged Out`;
+
+  res.send(`341CSE Team 6 Project - ${loginStatus}`);
+});
+
+app.get(
+  '/github/callback',
+  passport.authenticate('github', {
+    failureRedirect: '/api-docs',
+    session: false,
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/');
+  }
+);
+
+// Uncaught Exception Handler
+process.on('uncaughtException', (err, origin) => {
+  console.log(
+    process.stderr.id,
+    `Caught exception: ${err}\n` + `Exception origin: ${origin}`
+  );
+});
+
+// init MongoDB
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Database is listening and node is running on port ${port}`);
+    });
+  }
+});
