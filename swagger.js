39f722a87da54427ed1010a8308d9fd27b6805b8
sil <narradoresdigitales@gmail.com>
2025-06-09 05:05:49 -0500

    changed .env swagger values for local development

--- swagger.js
+++ swagger.js
@@ -5,8 +5,12 @@ const doc = {
     title: "Event Management Api",
     description: "Event Management Api",
   },
-  host: "three41-cse-team6-project.onrender.com",
-  schemes: ["https"],
+  // COMMENT OUT LOCAL HOST TO PUBLISH ON RENDER.COM
+  //host: "three41-cse-team6-project.onrender.com",
+  host: "localhost:3000",
+  schemes: ["http"],
+  // COMMENT OUT HTTP TO PUBLISH ON RENDER.COM
+  //schemes: ["https"],
 };
 
 const outputFile = "./swagger.json";
